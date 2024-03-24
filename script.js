c05475e0b5dc20222748070dfdf89c09e4ae0af5
imxaander <xander22.ison@gmail.com>
2024-03-24 22:27:12 +0800

    asd

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ $(document).ready(function() {
 
 });
 
+const myModalEl = document.getElementById('detail-modal')
+myModalEl.addEventListener('hidden.bs.modal', event => {
+  document.getElementById('detail-modal-trailer').innerHTML = ""
+})
+
 
 if(!("favs" in localStorage)){
     localStorage.setItem("favs", "")
@@ -55,9 +60,11 @@ function search(){
         wrapper.innerHTML = "";
   
         for(let child of ul.children){
-
+            console.log("size: " + ul.children.length);
             let itemDiv = document.createElement("div");
+        
             let href = child.querySelector('a').getAttribute("href").slice(14);
+            console.log(child.querySelector('a').getAttribute("href"));
             let name = child.querySelector('h3').innerHTML;
             let img = child.querySelector('a').querySelector('img').getAttribute("data-original");
             let itemImg = document.createElement("img");
@@ -86,8 +93,8 @@ function search(){
     }
     );
 }
-
-function detail(id, name, img){
+//get image done here remove
+function detail(id, name){
     $('#detail-modal').modal('toggle'); 
     document.querySelector("#detail-modal-title").innerHTML = name;
     // document.querySelector(".fav-button").setAttribute("onclick", `fav(\`${name}\`,\`${img}\`, \`${id}\` )`)
@@ -120,6 +127,7 @@ function detail(id, name, img){
         // console.log(details.innerText)
 
         //get on the top check it and implement the fogging shieee
+        var img = page.getElementsByClassName('img')[0].querySelector('img').getAttribute("src")
 
         document.querySelector(".fav-button").setAttribute("onclick", `fav(\`${name}\`,\`${img}\`, \`${id}\` )`)
         var tab = document.createElement("div");
@@ -127,6 +135,18 @@ function detail(id, name, img){
 
         var ul = tab.querySelector("ul");
 
+        try{
+            let trailer = page.querySelector('.trailer').children[1]
+            document.getElementById('detail-modal-trailer').innerHTML = `
+            <iframe src="${trailer.getAttribute("src")}" frameborder="0" width="1280" height="480" > </iframe>
+            
+            `
+
+        }catch(x){
+            console.log(x);
+            document.getElementById('detail-modal-trailer').innerHTML = "Trailer Doesnt Exist"
+
+        }
         
         checkFav(img);
         wrapper.innerHTML = "";
@@ -152,7 +172,7 @@ function detail(id, name, img){
             }
             itemR = `
             <div class="detail-modal-episode-item m-2 d-flex flex-wrap justify-content-between " onclick="watch('${href}')"> 
-                <div class="h4">Ep ${number} </div> <div class=""> ${fulldatetime}</div>
+                <div class="h4">${number} </div> <div class=""> ${fulldatetime}</div>
             </div>
             <hr>
             `
@@ -179,8 +199,12 @@ function watch(id){
         page.innerHTML = html;
         var title = page.querySelector("strong").innerHTML;
         document.querySelector(".video-title").innerHTML = title;
+        
         var url = page.getElementsByClassName("watch_video watch-iframe")[0].querySelector("iframe").getAttribute("src");
-        console.log(url);
+        var dlUrl = page.getElementsByClassName('download')[0].children[0].getAttribute("href")
+        console.log(dlUrl);
+        document.getElementById("watch-dl").href = "https:" + dlUrl
+        // console.log(url);
         wrapper.innerHTML = `<iframe id="video-player-iframe" sandbox = "allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation allow-modals" width="100%" height="100%" allowfullscreen src="${url}" target="_blank"></iframe>`;
     });
     var cssLink = document.createElement("link");
@@ -190,6 +214,8 @@ function watch(id){
 }
 
 function closeWatch(){
+    var wrapper = document.getElementsByClassName("video-player")[0];
+    wrapper.innerHTML = ""
     document.getElementById("video-pane").classList.add("visually-hidden")
 }
 
@@ -244,6 +270,12 @@ function refreshFav(){
         
         wrapper.appendChild(itemDiv);
     }
+
+    $('.fav-section-carousel').flickity({
+        // options
+        cellAlign: 'left',
+        contain: true
+    });
 }
 
 function getTitle(str) {
@@ -319,22 +351,63 @@ function showPopular(){
             page.remove();
         }
         
-    )
+    ).then(function(response){
+        $('.popular-section-carousel').flickity({
+            // options
+            cellAlign: 'left',
+            contain: true
+        });
+    })
 }
 
+function showIdk(){
+    let site = 'https://runasian.net'
+    var wrapper = $('.popular-section-carousel')[0];
+    wrapper.innerHTML = '<i class="fas fa-spinner spinner"></i>';
+    fetch(site).then(response => response.text()).then(html =>{
+        var page = document.createElement('html');
+        page.innerHTML = html;
+        console.log(page);
+        var slider = page.querySelector('.ls-wp-container')
+        console.log(slider);
+        var rndN = Math.floor(Math.random() * slider.children.length)
+        console.log(slider.children[rndN]);
+        var selectedSlide = slider.children[rndN]
+        var img = selectedSlide.getElementsByClassName('ls-bg')[0]
+        var a = selectedSlide.getElementsByClassName('ls-link')[0]
+        $('#idk-img').attr("src", img.getAttribute('src'))
+        $('#idk-title').html(img.getAttribute('title'))
+        $('#idk-btn').attr("onclick", `detail('${a.getAttribute("href").slice(14)}','${img.getAttribute("title")}', '${img.getAttribute('src')}')`)
+    })
+}
 
+// function searchDetails(name){
+//     let site = 'https://runasian.net/search?type=movies&keyword='+name
+//     fetch(site).then(response => response.text()).then(html =>{
+//         var page = document.createElement('html');
+//         page.innerHTML = html;
+
+//         var ul = page.getElementsByClassName('switch-block list-episode-item')[0];
+//         var detailsObj ={
+//             name: ul.children[0].querySelector('h3').innerHTML,
+//             img: ul.children[0].querySelector('a').querySelector('img').getAttribute("data-original"),
+//             href: ul.children[0].querySelector('a').getAttribute("href").slice(14)
+//         }
+//         console.log(detailsObj)
+//         return detailsObj
+//     })
+// }
 
 refreshFav()
 showPopular()
+showIdk()
+// setTimeout(()=>{
 
-setTimeout(()=>{
-    $('.popular-section-carousel').flickity({
-        // options
-        cellAlign: 'left',
-        contain: true
-    });
     
-}, 500)
+// }, 500)
 
-
-
+fetch('https://pladrac.net/download?id=NDAzNDI0&typesub=runasian-SUB&title=Wedding Impossible (2024) Episode 1').then(response => response.text()).then(html =>{
+    var page = document.createElement('html');
+        page.innerHTML = html;
+        console.log(page);
+})
